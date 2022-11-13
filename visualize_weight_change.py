5a136ba2d729f321f5e30653cc89e0aed39c48a1
sw801733 <sw801733@naver.com>
2022-11-13 11:53:44 +0900

    visualize every weight with matplotlib

---
+++ visualize_weight_change.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# loadtxt Weight hidden2 to output
+Wh2_out = np.loadtxt("./Wh2_out.txt")
+Wh2_out_1 = [Wh2_out[:,0], Wh2_out[:,1], Wh2_out[:,2]]
+Wh2_out_2 = [Wh2_out[:,3], Wh2_out[:,4], Wh2_out[:,5]]
+Wh2_out_3 = [Wh2_out[:,6], Wh2_out[:,7], Wh2_out[:,8]]
+
+# loadtxt Weight hidden1 to hidden2
+Wh1_h2 = np.loadtxt("./Wh1_h2.txt")
+Wh1_h2_1 = [Wh1_h2[:,0], Wh1_h2[:,1], Wh1_h2[:,2]]
+Wh1_h2_2 = [Wh1_h2[:,3], Wh1_h2[:,4], Wh1_h2[:,5]]
+Wh1_h2_3 = [Wh1_h2[:,6], Wh1_h2[:,7], Wh1_h2[:,8]]
+Wh1_h2_4 = [Wh1_h2[:,9], Wh1_h2[:,10], Wh1_h2[:,11]]
+Wh1_h2_5 = [Wh1_h2[:,12], Wh1_h2[:,13], Wh1_h2[:,14]]
+
+# loadtxt Weight input to hidden1
+Win_h1 = np.loadtxt("./Win_h1.txt")
+Win_h1_1 = [Win_h1[:,0], Win_h1[:,1], Win_h1[:,2]]
+Win_h1_2 = [Win_h1[:,3], Win_h1[:,4], Win_h1[:,5]]
+Win_h1_3 = [Win_h1[:,6], Win_h1[:,7], Win_h1[:,8]]
+
+
+# draw Weight hidden2 to output with 3 figures
+# for i in range(1,4):
+#     plt.figure()
+#     plt.xlabel('Number of Learning')
+#     plt.title(f'Weight Hidden2 node {i} to Output nodes')
+#     for j in range(3):
+#         plt.plot(eval('Wh2_out_' + str(i))[j], label=f"Wh2_out_{i}{j+1}")
+#         plt.legend()
+
+
+# draw Weight hidden1 to hidden2 with 5 figures
+# for i in range(1,6):
+#     plt.figure()
+#     plt.xlabel('Number of Learning')
+#     plt.title(f'Weight Hidden1 node {i} to Hidden2 nodes')
+#     for j in range(3):
+#         plt.plot(eval('Wh1_h2_' + str(i))[j], label=f"Wh1_h2_{i}{j+1}")
+#         plt.legend()
+
+
+# draw Weight input to hidden1 with 3 figures
+# for i in range(1,4):
+#     plt.figure()
+#     plt.xlabel('Number of Learning')
+#     plt.title(f'Weight Input node {i} to Hidden1 nodes')
+#     for j in range(3):
+#         plt.plot(eval('Win_h1_' + str(i))[j], label=f"Win_h1_{i}{j+1}")
+#         plt.legend()
+
+
+plt.show()
